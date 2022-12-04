0855d84af56674861fe90aabece98c13a14c7725
Daniel Tello Gaete <dtellogaete@gmail.com>
2022-12-03 23:49:09 -0600

    update desafio 2

--- assets/js/index-two.js
+++ assets/js/index-two.js
@@ -1,13 +1,17 @@
 const checkStickers = () => {
 
-    const github = Number(document.querySelector('#sticker-github').value);
-    const slack = Number(document.querySelector('#sticker-slack').value);
-    const git = Number(document.querySelector('#sticker-git').value);
+    const github = Number(document.querySelector('#sticker-github').value) || 0;
+    const slack = Number(document.querySelector('#sticker-slack').value) || 0;
+    const git = Number(document.querySelector('#sticker-git').value) || 0;
 
     const total = github + slack + git;    
 
-    if(total <= 10){     
-        document.querySelector("#text-sticker").innerHTML = total;        
+    if(total <= 10){
+        if(total <= 1){
+            document.querySelector("#text-sticker").innerHTML = "Llevas " + total + " sticker";
+        }else{
+            document.querySelector("#text-sticker").innerHTML = "Llevas " + total + " stickers";  
+        }              
     } else{
         document.querySelector("#text-sticker").innerHTML = "Llevas demasiados stickers"; 
     }  
